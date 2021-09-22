f5965db6231d235020c480898a979bc1bbb32649
gectokot <dmitry.kulak11@gmail.com>
2021-09-22 22:51:23 +0300

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const reviews = Array.from(document.querySelectorAll('.review-text'))
+
+const croppedReviews = reviews.map(review => [
+  review.innerHTML.slice(0, 200),
+  review.innerHTML.slice(200, review.innerHTML.length)])
+
+for (let i = 0; i <= 2; i++){
+  console.log(reviews[i].innerHTML)
+  reviews[i].innerHTML = croppedReviews[i][0] + '...'
+}
+
+const showFullText = (review, croppedReview) => {
+  review.innerHTML = croppedReview[0] + croppedReview[1];
+  review.classList.remove('cropped')
+}
+
+for (let i = 0; i <= 2; i++){
+  reviews[i].addEventListener('click',
+    () => showFullText(reviews[i], croppedReviews[i]))
+}
